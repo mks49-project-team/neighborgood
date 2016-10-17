06c719f87d1407e53a455d6db226c9134b45018a
Jeffrey Yoo <jeffyoo216@gmail.com>
2016-10-17 14:51:19 -0700

    [feature] beginning the setup, prior to setting up googlemaps

--- client/app/app.factory.js
+++ client/app/app.factory.js
@@ -1,9 +1,16 @@
 var factoryModule = angular.module('app.factory', []);
 
 factoryModule.factory('mainFactory', function($http){
-  var a = 2;
-  return {
-    a: a
-  }
 
+
+  // var getCommute = function(secondaryAddress) {
+  //   return $http({
+  //     method: "GET",
+  //     url:
+  //   })
+  // }
+
+  return {
+    getCommute: getCommute
+  }
 });
