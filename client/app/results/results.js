2564213838a8075922e1895ea5a3e4f983c75c92
sergysurge <sergysarkisyan@gmail.com>
2016-10-18 14:15:43 -0700

    [refactor] refactored some code, made startup page load on arriving to localhost, added redirect to options on startup submit, working on rendering map

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -3,58 +3,59 @@ var results = angular.module('results', ['app.factory']);
 results.controller('resultsController', function(mainFactory){
   var vm = this;
 
-  var mapinit= function() {
+  window.initMap = function() {
   	console.log('hi!')
-  	vm.getMapData();
+  	//vm.getMapData();
   }
 
-  var getMapData = function() {
+  vm.getMapData = function() {
+    var resPath = mainFactory.user.result;
   	var userCenter = {
-  		lat: mainFactory.user.result.newAddress.lat,
-  		lng: mainFactory.user.result.newAddress.lng
+  		lat: resPath.newAddress.lat,
+  		lng: resPath.newAddress.lng
   	};
 
   	var map = new google.maps.Map(document.getElementById('map'), {
   		zoom: 4,
-  		center: userCenter,
-  		backgroundColor: 'pink',
-  		scrollwheel: false
+  		center: userCenter
+  		//backgroundColor: 'pink',
+  		//scrollwheel: false
   	})
 
   	//universalMarkerMaker 2 params, type (store, restaurant) and data
   	var universalMarkerMaker = function(type) {
   		var storeCenter = {
-  		lat: mainFactory.user.result.nearbyStores.lat,
-  		lng: mainFactory.user.result.nearbyStores.lng
+  		lat: resPath.nearbyStores.lat,
+  		lng: resPath.nearbyStores.lng
   		}
   			/// DID NOT SET UP THE NEARBY STUFF RIGHT, THERE SHOULD BE A [] ARRAY
   		var restaurantCenter = {
-  		lat: mainFactory.user.result.nearbyRestaurants.lat,
-  		lng: mainFactory.user.result.nearbyRestaurants.lng
+  		lat: resPath.nearbyRestaurants.lat,
+  		lng: resPath.nearbyRestaurants.lng
   		}
 
   		if (type === 'store') {
-  			mainFactory.user.result.nearbyStores.forEach(
+  			resPath.nearbyStores.forEach(
   				function(item){
   					var marker = new google.maps.Marker({
   						position: storeCenter,
   						map: map,
-  						icon: mainFactory.user.result.nearbyStores.icon
+  						icon: resPath.nearbyStores.icon
   					})
   				})
   		} else if (type === 'restaurant') {
-  			mainFactory.user.result.nearbyRestaurants.forEach(
+  			resPath.nearbyRestaurants.forEach(
   				function(item){
   					var marker = new google.maps.Marker({
   						position: restaurantCenter,
   						map: map,
-  						icon: mainFactory.user.result.nearbyRestaurants.icon
+  						icon: resPath.nearbyRestaurants.icon
   					})
   				})
   		}
   	}
-  	//universalMarkerMaker('store')
-  	//universalMarkerMaker('restaurant')
+  	universalMarkerMaker('store')
+  	universalMarkerMaker('restaurant')
   }
   console.log('inside resultsController');
 });
