f02adce4b8dab8e7a87becf4ea0ecec1c3046c1b
Lucy Ji <lucyji1000@gmail.com>
2016-10-20 15:54:49 -0700

    [refactor] refactor graphing from angular-chart to c3 library

--- client/app/results/scores.js
+++ client/app/results/scores.js
@@ -1,24 +1,66 @@
-var scores = angular.module('scores', ['app.factory', 'angularChart']);
+var scores = angular.module('scores', ['app.factory']);
 
 scores.controller('scoresController', function(mainFactory){
   var vm = this;
-  vm.options = {
-    data: [
-      {
-        sales: 130,
-        income: 250
-      }
-    ],
-    dimensions: {
-      sales: {
-        type: 'bar'
-      },
-      income: {
-        axis: 'y2'
-      }
-    }
+  vm.chart = null;
+  var showChart = function(){
+    vm.chart = c3.generate({
+        bindto: '#scores-chart',
+        data: {
+          columns: [['data1', 50, 100, 60]],
+          type: 'bar'
+        },
+        bar: {
+          width: {
+            ratio: 0.8
+          }
+        }
+    })
   }
+  showChart();
+  // vm.options = {
+  //   data: [
+  //     {
+  //       safety: 80
+  //     },
+  //     {
+  //       walkability: 60
+  //     },
+  //     {
+  //       commute: 70
+  //     }
+  //   ],
+  //   dimensions: {
+  //     safety: {
+  //       type: 'bar',
+  //       dataType: 'numeric',
+  //       show: true
+  //     },
+  //     walkability: {
+  //       type: 'bar',
+  //       dataType: 'numeric',
+  //       show: true
+  //     },
+  //     commute: {
+  //       type: 'bar',
+  //       dataType: 'numeric',
+  //       show: true
+  //     }
+  //   },
+  //   chart: {
+  //
+  //   },
+  //   state: {
+  //
+  //   }
+  // }
   // vm.instance = null;
+  // vm.instance = chart.flow({
+  //   columns: [
+  //     ['safety', 'commute'],
+  //     ['data1', 50, 60]
+  //   ]
+  // });
   // vm.scores = 90;
   // var init = function(){
   //   vm.scores = mainFactory.getUserData().result.neighborhoodResult;
