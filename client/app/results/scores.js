0df1f9b08755f8b6bf94edc4e076b617645402fc
Lucy Ji <lucyji1000@gmail.com>
2016-10-20 23:31:06 -0700

    [feature] implemented color coding of score bars

--- client/app/results/scores.js
+++ client/app/results/scores.js
@@ -4,13 +4,29 @@ scores.controller('scoresController', function(mainFactory){
   var vm = this;
   vm.chart = null;
 
-
+  var setColors = function(chartData){
+    console.log('chartdata: ', chartData);
+    //var colors = {};
+    return chartData.reduce(function(colors, category){
+      console.log(colors);
+      if(category[1] < 40){
+        colors[category[0]] = '#ff0000';
+      } else if (category[1] >= 40 && category[1] < 75) {
+        colors[category[0]] = 'ff6600';
+      } else {
+        colors[category[0]] = '009900';
+      }
+      return colors;
+    }, {});
+  }
   var init = function(){
     //var scores = mainFactory.getUserData().result.neighborhoodResult;
+    //var neighborhood = mainFactory.getUserData().result.newAddress.neighborhood;
     // generate data for bar chart
-    var scores = {safety: 40, walk: 80, total: 76};
+    var neighborhood = 'la'
+    var scores = {safety: 40, walk: 3, total: 76};
     var chartData = [];
-//    var chartTotalColumn;
+
     for(var key in scores){
         chartData.push([key, scores[key]]);
     }
@@ -31,7 +47,9 @@ scores.controller('scoresController', function(mainFactory){
     var chartBlankData = chartData.map(function(category){
       return [category[0], 0];
     });
-//    chartBlankData.push(['total', 0]);
+
+    // generate bar colors based on scores-chart
+    var chartColors = setColors(chartData);
 
     vm.chart = c3.generate({
         bindto: '#scores-chart',
@@ -41,27 +59,41 @@ scores.controller('scoresController', function(mainFactory){
         },
         data: {
           columns : chartBlankData,//chartData,
-          type: 'bar'
-        },
-        bar: {
-          width: {
-            ratio: 0.5
-          }
+          type: 'bar',
+          colors: chartColors,
         },
         axis: {
+          x: {
+            type: 'category',
+            padding: {
+              left: 0,
+              right: 0
+            },
+            tick:{
+              count: 0
+            }
+          },
           y: {
+            tick: {
+              count: 5
+            },
+            min: 0,
+            max: 100,
+            padding: 0,
             label: {
               text: 'Score',
               position: 'outer-middle'
             }
           }
+        },
+        tooltip: {
+          grouped: false,
+          format: {
+            title: function(x) {return neighborhood;}
+          }
         }
     });
 
-    vm.chart.data.colors({
-      'safety': '#00000',
-      'a': '#00000'
-    });
     setTimeout(function(){
       vm.chart.load({
         columns: chartData
