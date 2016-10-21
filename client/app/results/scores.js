85f089e40ebe7077dbb816d731d0c20ac696c841
Lucy Ji <lucyji1000@gmail.com>
2016-10-20 22:23:16 -0700

    [feature] implemented sorting of columns for bar chart

--- client/app/results/scores.js
+++ client/app/results/scores.js
@@ -4,16 +4,21 @@ scores.controller('scoresController', function(mainFactory){
   var vm = this;
   vm.chart = null;
 
+
   var init = function(){
     //var scores = mainFactory.getUserData().result.neighborhoodResult;
     // generate data for bar chart
-    var scores = {safety: 40, walk: 80, a: 90};
+    var scores = {safety: 40, walk: 80, total: 76};
     var chartData = [];
+//    var chartTotalColumn;
     for(var key in scores){
-      chartData.push([key, scores[key]]);
+        chartData.push([key, scores[key]]);
     }
     // sort categories so they always appear in same order
     chartData.sort(function(a,b){
+      if((a[0] == 'total') != (b[0] == 'total')){ // keep total score as last bar
+        return a[0] =='total' ? 1 : -1;
+      }
       if (a[0] < b[0]) {
         return -1;
       }
@@ -22,15 +27,20 @@ scores.controller('scoresController', function(mainFactory){
       }
       return 0;
     });
-    console.log(chartData);
+
+    var chartBlankData = chartData.map(function(category){
+      return [category[0], 0];
+    });
+//    chartBlankData.push(['total', 0]);
+
     vm.chart = c3.generate({
         bindto: '#scores-chart',
         size: {
-          width: 300,
-          height: 300
+          width: 250,
+          height: 200
         },
         data: {
-          columns : chartData,
+          columns : chartBlankData,//chartData,
           type: 'bar'
         },
         bar: {
@@ -49,8 +59,14 @@ scores.controller('scoresController', function(mainFactory){
     });
 
     vm.chart.data.colors({
-
-    })
+      'safety': '#00000',
+      'a': '#00000'
+    });
+    setTimeout(function(){
+      vm.chart.load({
+        columns: chartData
+      })
+    }, 250);
   }
   init();
 
