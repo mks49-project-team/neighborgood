a2f3be62b0fc3b5606c1444ace7f00c7dd0f3499
Lucy Ji <lucyji1000@gmail.com>
2016-10-20 20:40:42 -0700

    [feature] added controllers and factory functions for authentication

--- client/app/signin/signin.js
+++ client/app/signin/signin.js
@@ -1,9 +1,23 @@
-var signin = angular.module('signin', []);
+var signin = angular.module('signin', ['app.factory.js']);
 
-signin.controller('signinController', function(){
+signin.controller('signinController', function(authentication, $location){
   var vm = this;
+  vm.user = {};
+  vm.showErrorMsg = false;
+
+  var init = function() {
+    authentication.signout();
+  }
 
   vm.signin = function(){
-    
+    authentication.signin(vm.user, function(success){
+      if(success) {
+        $location.path('/');
+      } else {
+        vm.error = true;
+      }
+    });
   }
+  init();
+
 });
