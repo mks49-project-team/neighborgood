df03fe696fc872d558bfe58393ae7ea3aaffd3fd
Lucy Ji <lucyji1000@gmail.com>
2016-10-22 17:43:31 -0700

    [style] added styling for startup view

--- client/app/nav/nav.js
+++ client/app/nav/nav.js
@@ -1,16 +1,11 @@
 var nav = angular.module('nav', ['app.factory'])
 nav.controller('navController', function($window, $location, userFactory){
   var vm = this;
-  vm.isUserLoggedIn = false;
+  vm.isUserLoggedIn = $window.localStorage.user && $window.localStorage.token;
 
-  var init = function(){
-    if($window.localStorage.user && $window.localStorage.token){
-      vm.isUserLoggedIn = true;
-    }
-  }
-  init();
+
   vm.signout = function(){
     userFactory.signout();
-    $location.path('/signin');
+    $location.path('signin');
   }
 });
